02e28510369f6c60bb4d7ea6fa2247808fa98226
Tjerk Woudsma <tjerk@studiomoniker.com>
2018-04-11 23:32:49 +0200

    cleanup

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,12 @@
 // Exporting a function
 export default (p) => {
+
   // Use Processing functions with p5.js
   p.setup = () => {
     // Creating a canvas using the entire screen of the webpage
     p.createCanvas(window.innerWidth, window.innerHeight)
     p.noStroke()
+    p.rectMode(p.CENTER)
 
     console.log('p5.js setup function executed')
   }
@@ -14,17 +16,10 @@ export default (p) => {
     p.background(255)
 
     // Draw a rectangle
+    var w = p.random(100, 400)
+    var h = p.random(100, 400)
     p.fill(0)
-    p.rect(p.width / 2, p.height / 2, 80, 80)
-
-    // Draw a funky ellipse
-    p.fill(255, 0, 0)
-    p.funkyEllipse(200, 200, 100, 100)
+    p.ellipse(p.width / 2, p.height / 2, w, h)
   }
 
-  p.funkyEllipse = (x, y, w, h) => {
-    w = w + p.random(-10, 10)
-    h = h + p.random(-10, 10)
-    p.ellipse(x, y, w, h)
-  }
 }
